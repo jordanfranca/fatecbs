b3c5154f3f4a86c8ae508eaefba8ee21380bd149
jordanfranca <jordan1992_16@hotmail.com>
2015-03-23 01:02:54 -0300

    Model Alunos
    Comentários e melhorias

--- application/models/alunos_model.php
+++ application/models/alunos_model.php
@@ -1,11 +1,21 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 	/**
 	* Modelo de Alunos
+	* @autor Jordã França
 	*
 	**/
 	class Alunos_model extends CI_MODEL {
+
+		private $table = 'tb_alunos';
+
+		//Construtor
 		function __construct() {
         	parent::__construct();
+    	}
+
+    	//Retorna o nome da tabela
+    	function getTable() {
+    		return $this->table;
     	}
 
     	/**
@@ -16,30 +26,35 @@
         	return $this->db->insert('tb_alunos', $data);
     	}
 
+    	//Listar os alunos
     	function listar($intMaximo, $intInicio) {
     		$this->db->select('tb_cursos.nm_curso, tb_alunos.cd_aluno, tb_alunos.cd_curso, tb_alunos.ds_ra,ds_nome, tb_alunos.ds_cpf,tb_alunos.ds_email_pessoal, tb_alunos.ds_email_corporativo');
     		$this->db->join('tb_cursos', 'tb_cursos.cd_curso = tb_alunos.cd_curso', 'left');
-			$query = $this->db->get('tb_alunos', $intMaximo, $intInicio);
+			$query = $this->db->get($this->getTable(), $intMaximo, $intInicio);
 			return $query->result();
 		}
 
+		//contar registros
 		function contaRegistros() {
-			return $this->db->count_all_results('tb_alunos');
+			return $this->db->count_all_results($this->getTable());
 		}
 
+		//Buscar aluno pelo ID
 		function getAluno($id) {
 			$this->db->where('cd_aluno', $id);
-			$query = $this->db->get('tb_alunos');
+			$query = $this->db->get($this->getTable());
 			return $query->row();
 		}
 
+		//Atualizar aluno
 		function update($data,$id) {
 			$this->db->where('cd_aluno', $id);
-			return $this->db->update('tb_alunos', $data);
+			return $this->db->update($this->getTable(), $data);
 		}
 
+		//Deletar Aluno
 		function delete($id) {
 			$this->db->where('cd_aluno', $id);
-			return $this->db->delete('tb_alunos');
+			return $this->db->delete($this->getTable());
 		}
 	}
